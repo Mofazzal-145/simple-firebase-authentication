9f4a6d4c9509bc7e165b0eb92af33deed9de4a9e
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-06 17:42:06 +0600

    sign out button

--- src/_steps.js
+++ src/_steps.js
@@ -1,14 +1,14 @@
 /* 
 ** Steps to use firebase 
 * 1. Create a project on console.firebase.google.com
-* 2. install firebase
+* 2. npm install firebase
 * 3. Register Wep app in firebase
 * 4. copy firebase init with config from firebase project settings into a file firebase.inti.js
 * 5. export default app
 * 6. Import app firebase init.js to app.js
 * 7. Import getAuth and use in app.js
 * 8. Turn on google authentication (firebase> authentication> enable google sign in)
-*
+* 9. Create google provider
 *
 *
 *
